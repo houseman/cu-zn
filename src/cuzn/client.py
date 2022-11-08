7fe9e39d391a0c08a7d51555b82975b130f71ca3
Scott Houseman <scott.houseman@takealot.com>
2022-11-08 22:54:26 +0200

    nox integration

--- src/cuzn/client.py
+++ src/cuzn/client.py
@@ -1,6 +1,8 @@
 import os
 from typing import Optional
 
+from .errors import ConfigurationError
+
 
 class Client:
     _endpoint: Optional[str]
@@ -12,8 +14,8 @@ class Client:
         braze_endpoint: Optional[str] = None,
         braze_api_key: Optional[str] = None
     ) -> None:
-        self._endpoint = braze_endpoint or os.getenv("BRAZE_ENDPOINT")
-        self._api_key = braze_api_key or os.getenv("BRAZE_API_KEY")
+        self._endpoint = braze_endpoint or os.getenv("CUZN_BRAZE_ENDPOINT")
+        self._api_key = braze_api_key or os.getenv("CUZN_BRAZE_API_KEY")
 
         if self._endpoint is None or self._api_key is None:
-            raise Exception("Could not configure a Braze client")
+            raise ConfigurationError("Could not configure a Braze client")
